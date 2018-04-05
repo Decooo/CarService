94d10d8b6ac3102bda401712aa2159070580e2ad
Jakub Kuśnierz <jakubkusnierz@gmail.com>
2018-04-05 03:44:10 +0200

    Walidacja rejestracji

--- SpringBoot/src/main/java/com/serwis/controller/RegistrationController.java
+++ SpringBoot/src/main/java/com/serwis/controller/RegistrationController.java
@@ -8,13 +8,17 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 /**
@@ -45,6 +49,7 @@ public class RegistrationController implements Initializable {
 	}
 
 	private ObservableList<String> doListRole() {
+		listRole.clear();
 		listRole.add("Serwisant");
 		listRole.add("Magazynier");
 		listRole.add("Administrator");
@@ -53,6 +58,7 @@ public class RegistrationController implements Initializable {
 	}
 
 	private void doMapRole() {
+		map.clear();
 		map.put("Serwisant", "SERVICEMAN");
 		map.put("Magazynier", "WAREHOUSEMAN");
 		map.put("Administrator", "ADMINISTRATOR");
@@ -60,10 +66,53 @@ public class RegistrationController implements Initializable {
 
 	@FXML
 	public void addNewAccountAction(ActionEvent event) {
-		Users newUser = new Users();
-		newUser.setUsername(textFieldUsername.getText());
-		newUser.setPassword(textFieldPassword.getText());
-		newUser.setRole(map.get(chooseRole.getSelectionModel().getSelectedItem()));
-		usersService.addUser(newUser);
+		if (textFieldUsername.getText().length() < 3) {
+			incorrectUsername();
+		} else if (textFieldPassword.getText().length() < 5) {
+			incorrectPassword();
+		} else {
+			Users newUser = new Users();
+			newUser.setUsername(textFieldUsername.getText());
+			newUser.setPassword(textFieldPassword.getText());
+			newUser.setRole(map.get(chooseRole.getSelectionModel().getSelectedItem()));
+			usersService.addUser(newUser);
+			alertAccountWasCreated();
+		}
 	}
+
+	private void alertAccountWasCreated() {
+	Alert alert = new Alert(Alert.AlertType.INFORMATION);
+	alert.setTitle("Założono nowe konto");
+	alert.setHeaderText("Pomyślnie założono nowe konto");
+	alert.getButtonTypes().setAll(ButtonType.OK);
+	Optional<ButtonType> result = alert.showAndWait();
+	if(result.get()==ButtonType.OK){
+		Stage stage = (Stage) textFieldPassword.getScene().getWindow();
+		stage.close();
+		}
+	}
+
+	private void incorrectUsername() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Bład zakładania konta");
+		alert.setHeaderText("Nazwa użytkownika musi mieć minimum 3 znaki");
+		alert.getButtonTypes().setAll(ButtonType.OK);
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK) {
+			textFieldUsername.clear();
+			textFieldPassword.clear();
+		}
+	}
+
+	private void incorrectPassword() {
+		Alert alert = new Alert(Alert.AlertType.INFORMATION);
+		alert.setTitle("Błąd zakładania konta");
+		alert.setHeaderText("Hasło musi mieć minimum 5 znaków");
+		alert.getButtonTypes().setAll(ButtonType.OK);
+		Optional<ButtonType> result = alert.showAndWait();
+		if (result.get() == ButtonType.OK) {
+			textFieldPassword.clear();
+		}
+	}
+
 }
