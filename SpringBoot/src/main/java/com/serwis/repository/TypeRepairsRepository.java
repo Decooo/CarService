687cb62214e6e892fc3299cd90ace53e1594082b
Jakub Kuśnierz <jakubkusnierz@gmail.com>
2018-05-18 13:04:44 +0200

    Dodanie TypeRepairs

---
+++ SpringBoot/src/main/java/com/serwis/repository/TypeRepairsRepository.java
@@ -0,0 +1,12 @@
+package com.serwis.repository;
+
+import com.serwis.entity.TypeRepairs;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by jakub on 18.05.2018.
+ */
+@Repository
+public interface TypeRepairsRepository extends JpaRepository<TypeRepairs,Integer>{
+}
